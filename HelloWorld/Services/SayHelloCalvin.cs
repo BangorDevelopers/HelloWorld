fe77ad8ebc00349ddfb4436b020dfe959dccee51
Calvin Bishop <calbis@gmail.com>
2012-04-24 19:57:40 -0400

    calvin says hi

---
+++ HelloWorld/Services/SayHelloCalvin.cs
@@ -0,0 +1,18 @@
+﻿using HelloWorld.ServiceInterfaces;
+
+namespace HelloWorld.Services
+{
+    public class SayHelloCalvin : ISayHello
+    {
+        public string Name
+        {
+            get { return "calvin"; }
+        }
+
+        public string SayHello()
+        {
+            return "Hello, My name is calvin!";
+        }
+        
+    }
+}
