86206fcbe4a7daec6f2fd7fc4ed6520306e425ef
Daniel Pratt <kolkata_is@hotmail.com>
2012-04-25 16:31:12 -0400

    converted project to use MEF

--- HelloWorld/ServiceInterfaces/ISayHello.cs
+++ HelloWorld/ServiceInterfaces/ISayHello.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.Composition;
 
 namespace HelloWorld.ServiceInterfaces
 {
+    [InheritedExport]
     public interface ISayHello
     {
         string Name { get; }
